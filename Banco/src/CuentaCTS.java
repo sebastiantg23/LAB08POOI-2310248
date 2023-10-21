067046c8a70a8fa454da224ac7ebe3fc72d3541d
alexchessknight <142682640+alexchessknight@users.noreply.github.com>
2023-10-21 12:40:41 -0500

    Add files via upload

---
+++ Banco/src/CuentaCTS.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alumno
+ */
+public class CuentaCTS {
+    private double tasaInteresAnual=0.07;
+    private double puntos;
+    
+    //Constructor
+
+    public CuentaCTS(double puntos) {
+        this.puntos = puntos;
+    }
+    
+
+    public double getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(double puntos) {
+        this.puntos = puntos;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaCTS{" + "tasaInteresAnual=" + tasaInteresAnual + ", puntos=" + puntos + '}';
+    }
+    
+    
+}
