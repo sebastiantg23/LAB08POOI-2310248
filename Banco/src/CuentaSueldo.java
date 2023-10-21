5b671e88281dfc35a390ab4f988aabd96dec7704
Sj2910 <142682922+Sj2910@users.noreply.github.com>
2023-10-21 12:19:31 -0500

    Add files via upload

---
+++ Banco/src/CuentaSueldo.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JESSICA
+ */
+public class CuentaSueldo {
+    
+    private String nombreEmpresa;
+    
+    //constructor
+
+    public CuentaSueldo(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+    
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaSueldo{" + "nombreEmpresa=" + nombreEmpresa + '}';
+    }
+
+    
+    
+    
+    
+    
+}
